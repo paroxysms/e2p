6742d3a73411dfc299f101235a952ab7e1001c00
paroxysms <alphaary23@gmail.com>
2025-03-31 11:46:27 -0500

    working, 0.9s benchmark

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use crate::perspective::Equirectangular;
+use opencv;
+
+mod perspective;
+
+fn main() {
+    let image = Equirectangular::new("image.jpg");
+    let perspective_image = image.get_perspective(60.0, 80.0, 33.0, 720, 1080);
+    opencv::imgcodecs::imwrite("final_image.jpg", &perspective_image, &opencv::core::Vector::<i32>::new()).expect("Could not write image!");
+}
